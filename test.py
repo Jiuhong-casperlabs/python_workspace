347c0a25dd06eaf1a4bc2ff7dac34041380e3a4f
Jiuhong <jiuhong@casperlabs.io>
2023-12-28 18:08:21 +0800

    u

--- test.py
+++ test.py
@@ -1,6 +1,18 @@
-with open("string.txt","r") as f:
-    emails = f.read().split(":")
-    print(len(emails))
+my_json={"a":1,"b":2,"c":3,"d":4,"e":5}
 
-[print(email) for email in emails]
+print(my_json.keys())
+print(type(my_json.keys()))
+print(list(my_json.keys()))
 
+print(my_json.values())
+print(type(my_json.values()))
+print(list(my_json.values()))
+
+print(my_json.items())
+print(type(my_json.items()))
+print(list(my_json.items()))
+
+x={name:age for name,age in enumerate(my_json.items())}
+print(x)
+x=dict(zip(my_json.keys(), my_json.values()))
+print(x)
